06a5693b60f35f74e366ca0cea6f95185cd54607
Bruno Borelli <brunoborellipro@gmail.com>
2021-07-02 19:26:57 -0300

    README.md

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # react-cotacao
-Uma aplicação simples feita com react que consome uma api para exibir dados sobre a cotação do dólar.
+Uma aplicação simples feita com react que consome uma api para exibir dados sobre a cotação do dólar e outras moedas.
+![](./react-cotacao.png)
--- src/dolarCalculator/dolarCalculator.tsx
+++ src/dolarCalculator/dolarCalculator.tsx
@@ -1,81 +1,81 @@
 import React from 'react'
 import api from '../axios/api'
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import formatNumber from '../service/formatNumbers'
 import { InputAdornment, Input } from '@material-ui/core';
 import './dolarCalculator.css'
 
 
-interface Cotacao{
-    code: string,
-    codein: string,
-    name: string,
-    high: string,
-    low: string,
-    varBid: string,
-    pctChange: string,
-    bid: string,
-    ask: string,
-    timestamp: string,
-    create_date: string,
+interface Cotacao {
+	code: string,
+	codein: string,
+	name: string,
+	high: string,
+	low: string,
+	varBid: string,
+	pctChange: string,
+	bid: string,
+	ask: string,
+	timestamp: string,
+	create_date: string,
 }
 
-function DolarCalculator(): JSX.Element{
+function DolarCalculator(): JSX.Element {
 
-    const [moedas, setMoedas] = useState<string[]>([])
-    const [moedasData, setMoedasData] = useState<{[key:string]:Cotacao}>()
-    const [moedaSelecionada, setMoedaSelecionada] = useState<Cotacao>()
-    const [valorEstrangeiro, setValorEstrangeiro] = useState<string>()
+	const [moedas, setMoedas] = useState<string[]>([])
+	const [moedasData, setMoedasData] = useState<{ [key: string]: Cotacao }>()
+	const [moedaSelecionada, setMoedaSelecionada] = useState<Cotacao>()
+	const [valorEstrangeiro, setValorEstrangeiro] = useState<string>()
 
-    useEffect(()=>{
-        getMoedas().then(
-            (response)=>{
-                const [moedas, moedasData] = response
-                setMoedas(moedas)
-                setMoedaSelecionada(moedasData![moedas[0]])
-                setMoedasData(moedasData)
-            }
-        )
-    },[])
+	useEffect(() => {
+		getMoedas().then(
+			(response) => {
+				const [moedas, moedasData] = response
+				setMoedas(moedas)
+				setMoedaSelecionada(moedasData![moedas[0]])
+				setMoedasData(moedasData)
+			}
+		)
+	}, [])
 
-    return(
-        <div className="container">
-            <div className="box">
-                <div className="ValorRealBox">
-                    <p className="ValorReal">R${
-                    moedaSelecionada && valorEstrangeiro ? formatNumber(2,Number(valorEstrangeiro)*Number(moedaSelecionada?.ask)) : "0,00"
-                    }</p>
-                </div>
-                <input className="CalculatorInput" type="number" onChange={
-                    (event)=>{
-                        const valor = event.target.value
-                        setValorEstrangeiro(valor)
-                    }
-                }></input>
+	return (
+		<div className="container">
+			<div className="box">
+				<div className="ValorRealBox">
+					<p className="ValorReal">R${
+						moedaSelecionada && valorEstrangeiro ? formatNumber(2, Number(valorEstrangeiro) * Number(moedaSelecionada?.ask)) : "0,00"
+					}</p>
+				</div>
+				<input className="CalculatorInput" type="number" onChange={
+					(event) => {
+						const valor = event.target.value
+						setValorEstrangeiro(valor)
+					}
+				}></input>
 
-                <select className="SelectMoedas" name="moeda" id="moeda" onChange={(event)=>{
-                    const moeda_selecionda = event.target.value
-                    setMoedaSelecionada(moedasData![moeda_selecionda])
-                    }}>
-                    {moedas.map((moeda)=>{
-                        return(<option value={moeda} key={moeda}>{moeda}</option>)
-                    })}
-                </select>
-                    
-            </div>
-        </div>
-        
-    )
+				<select className="SelectMoedas" name="moeda" id="moeda" onChange={(event) => {
+					const moeda_selecionda = event.target.value
+					setMoedaSelecionada(moedasData![moeda_selecionda])
+				}}>
+					{moedas.map((moeda) => {
+						return (<option value={moeda} key={moeda}>{moeda}</option>)
+					})}
+				</select>
+
+			</div>
+		</div>
+
+	)
 }
 
-async function getMoedas(){
-    const response = await api.get('')
-    var moedas:string[] = []
-    for (const moeda in response.data) {
-        moedas.push(moeda)
-    }
-    const moedasData = response.data
-    return [moedas, moedasData]
+async function getMoedas() {
+	const response = await api.get('')
+	var moedas: string[] = []
+	for (const moeda in response.data) {
+		moedas.push(moeda)
+	}
+	const moedasData = response.data
+	return [moedas, moedasData]
 }
 
 
