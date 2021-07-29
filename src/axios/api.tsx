84ca309229c4d2caee9f74b849f27ca755c282ec
Bruno Borelli <brunoborellipro@gmail.com>
2021-07-29 19:59:56 -0300

    atulizar chamada para api

--- src/axios/api.tsx
+++ src/axios/api.tsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create(
     {
-        baseURL: 'https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL,BRL-GBP,BRL-INR,BRL-JPY,BRL-MXN,BRL-CAD'
+        baseURL: 'https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BRL-GBP,BRL-INR,BRL-JPY,BRL-MXN,BRL-CAD'
     }
 )
 
